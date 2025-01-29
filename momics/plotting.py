30a3b158a2c1ca6f7cef3cea09d61555d5a91254
palec87 <david@stanka.de>
2025-01-29 18:46:25 +0000

    fixed labels and cat/numerical factors

--- momics/plotting.py
+++ momics/plotting.py
@@ -24,32 +24,54 @@ def plot_pcoa_black(pcoa_df: pd.DataFrame, color_by: str = None) -> plt.Figure:
     Returns:
         plt.Figure: The PCoA plot.
     """
+    flag_massage = False
     plot = plt.figure(figsize=(10, 6), facecolor=(0, 0, 0, 0))
     ax = plot.add_subplot(111)
 
     if color_by is not None:
-        scatter = plt.scatter(
-            pcoa_df["PC1"],
-            pcoa_df["PC2"],
-            c=pcoa_df[color_by],
-            cmap="RdYlGn",
-            edgecolor="k",
-        )
-        plt.colorbar(scatter, label=color_by)
+        labels = fold_legend_labels_from_series(pcoa_df[color_by], 35)
+        print(pcoa_df[color_by].dtype)
+        # BETA created now only for numerical
+        if pcoa_df[color_by].dtype == "object":
+            print('categorical')
+            sns.scatterplot(
+                data=pcoa_df,
+                x="PC1",
+                y="PC2",
+                hue=color_by,
+                palette="coolwarm",
+                edgecolor="black",
+            )
+            ax = change_legend_labels(ax, labels)
+        else:
+            print('numerical', pcoa_df[color_by].count(), len(pcoa_df[color_by]))
+            flag_massage = True
+            perc = pcoa_df[color_by].count() / len(pcoa_df[color_by]) * 100
+            scatter = plt.scatter(
+                pcoa_df["PC1"],
+                pcoa_df["PC2"],
+                c=pcoa_df[color_by],
+                cmap="RdYlGn",
+                edgecolor="k",
+            )
+            plt.colorbar(scatter, label=color_by)
     else:
+        print('single color')
         plt.scatter(pcoa_df["PC1"], pcoa_df["PC2"], color="black")
 
     ax.set_xlabel("PC1")
     ax.set_ylabel("PC2")
-    ax.set_title("PCoA Plot")
+    if flag_massage:
+        ax.set_title(f"PCoA Plot with valid {color_by} values: ({perc:.2f}%)")
+    else:
+        ax.set_title("PCoA Plot")
     plt.tight_layout()
     plt.close(plot)
     return plot
 
 
 def mpl_alpha_diversity(alpha_df: pd.DataFrame, factor: str = None) -> plt.Figure:
-    """
-    Plots the Shannon index grouped by a factor.
+    """Plots the Shannon index grouped by a factor.
 
     Args:
         alpha_df (pd.DataFrame): A DataFrame containing alpha diversity results.
@@ -60,7 +82,7 @@ def mpl_alpha_diversity(alpha_df: pd.DataFrame, factor: str = None) -> plt.Figur
     """
     alpha_df = alpha_df.sort_values(by=factor)
     plot = plt.figure(figsize=(10, 6), facecolor=(0, 0, 0, 0))
-    labels = [fill(x, 20) if isinstance(x, str) and len(x) > 30 else str(x) for x in alpha_df[factor].unique()]
+    labels = fold_legend_labels_from_series(alpha_df[factor], 35)
 
     ax = plot.add_subplot(111)
     sns.barplot(
@@ -69,13 +91,11 @@ def mpl_alpha_diversity(alpha_df: pd.DataFrame, factor: str = None) -> plt.Figur
         y="Shannon",
         hue=factor,
         palette="coolwarm",
-        )
+    )
 
     # check axes and find which is have legend
-    leg = ax.get_legend()
-    for t, l in zip(leg.texts, labels):
-        t.set_text(l)
-    
+    ax = change_legend_labels(ax, labels)
+
     ax.set_title(f"Shannon Index Grouped by {factor}")
     ax.set_xlabel("Sample")
     ax.set_ylabel("Shannon Index")
@@ -87,8 +107,7 @@ def mpl_alpha_diversity(alpha_df: pd.DataFrame, factor: str = None) -> plt.Figur
 
 
 def mpl_average_per_factor(df: pd.DataFrame, factor: str = None) -> plt.Figure:
-    """
-    Plots the average Shannon index grouped by a factor.
+    """Plots the average Shannon index grouped by a factor.
 
     Args:
         df (pd.DataFrame): A DataFrame containing alpha diversity results.
@@ -112,6 +131,7 @@ def mpl_average_per_factor(df: pd.DataFrame, factor: str = None) -> plt.Figure:
     ax.set_title(f"Average Shannon Index Grouped by {factor}")
     ax.set_xlabel(factor)
     ax.set_ylabel("Shannon Index")
+    ax = cut_xaxis_labels(ax, 15)
 
     plt.tight_layout()
     plt.close(plot)
@@ -145,7 +165,6 @@ def alpha_plot(
         mpl_alpha_diversity(alpha, factor=factor),
         sizing_mode="stretch_both",
         name="Alpha div",
-        
     )
     return fig
 
@@ -262,3 +281,56 @@ def mpl_plot_heatmap(df: pd.DataFrame, taxon: str) -> plt.Figure:
     plt.tight_layout()
     plt.close(plot)
     return plot
+
+
+####################
+# Helper functions #
+####################
+def fold_legend_labels_from_series(df: pd.Series, max_len: int = 30) -> List[str]:
+    """Folds a list of labels to a maximum length from a Series.
+
+    Args:
+        df (pd.Series): The series to extract unique labels.
+        max_len (int, optional): The maximum length of a label. Defaults to 30.
+
+    Returns:
+        List[str]: The folded list of labels.
+    """
+    return [
+        fill(x, max_len) if isinstance(x, str) and len(x) > max_len else str(x)
+        for x in df.unique()
+    ]
+
+
+def change_legend_labels(ax: plt.axis, labels: List[str]) -> plt.axis:
+    """Changes the labels of a legend on a given matplotlib axis.
+
+        ax (plt.axis): The matplotlib axis object whose legend labels need to be changed.
+        labels (List[str]): A list of new labels to be set for the legend.
+
+    Returns:
+        plt.axis: The matplotlib axis object with updated legend labels.
+    """
+    leg = ax.get_legend()
+    for t, l in zip(leg.texts, labels):
+        t.set_text(l)
+    return ax
+
+
+def cut_xaxis_labels(ax: plt.axis, n: int = 15) -> plt.axis:
+    """Changes the x-tick labels by cutting them short.
+
+    Args:
+        ax: The axes to change the x-axis of.
+        n: cutoff for max number of characters for the xtick label.
+
+    Returns:
+        plt.axis: The axes with the new x-tick labels.
+    """
+    ticks = ax.get_xticklabels()
+    new_ticks = []
+    for tick in ticks:
+        tick.set_text(tick.get_text()[: min(len(tick.get_text()), n)])
+        new_ticks.append(tick)
+    ax.set_xticklabels(new_ticks)
+    return ax
