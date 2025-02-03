557171d52d0b24b57783db8303852f809c09f16c
palec87 <david@stanka.de>
2025-02-03 17:30:04 +0000

    change

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ release = '0.0.1'
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx_autoapi'
+    'sphinx.ext.autoapi'
 ]
 # extensions.append('autoapi.extension')
 autoapi_dirs = ['../momics']
