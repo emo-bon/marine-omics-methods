cc845861e18285dde95dcf0cb50eadf35ecdd341
palec87 <david@stanka.de>
2025-04-08 17:40:53 +0100

    add gecco analyze convert bytes to df

--- momics/loader/__init__.py
+++ momics/loader/__init__.py
@@ -1,9 +1,11 @@
 from .parquets import load_parquets
 from .ro_crates import get_ro_crate_metadata_gh, get_ro_crate_data
+from .utils import bytes_to_df
 
 
 __all__ = [
     "get_ro_crate_metadata_gh",
     "get_ro_crate_data",
     "load_parquets",
+    "bytes_to_df",
 ]
