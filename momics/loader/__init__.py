f336d8be1c557d9e03bfbdd1723a0d30b1053116
palec87 <david@stanka.de>
2025-05-20 16:29:42 +0100

    debug

--- momics/loader/__init__.py
+++ momics/loader/__init__.py
@@ -1,11 +1,11 @@
 from .parquets import load_parquets
-from .ro_crates import get_ro_crate_metadata_gh, get_ro_crate_data
+from .ro_crates import get_rocrate_metadata_gh, get_rocrate_data
 from .utils import bytes_to_df
 
 
 __all__ = [
-    "get_ro_crate_metadata_gh",
-    "get_ro_crate_data",
+    "get_rocrate_metadata_gh",
+    "get_rocrate_data",
     "load_parquets",
     "bytes_to_df",
 ]
