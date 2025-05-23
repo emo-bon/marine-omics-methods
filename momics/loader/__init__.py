4dd71507e73009c6353d67a770a6c486efc928bb
palec87 <david@stanka.de>
2025-05-23 15:34:01 +0100

    expose new methods to import

--- momics/loader/__init__.py
+++ momics/loader/__init__.py
@@ -1,4 +1,4 @@
-from .parquets import load_parquets
+from .parquets import load_parquets, load_parquets_udal
 from .ro_crates import (
     get_rocrate_metadata_gh,
     get_rocrate_data,
@@ -14,5 +14,6 @@ __all__ = [
     "extract_data_by_name",
     "extract_all_datafiles",
     "load_parquets",
+    "load_parquets_udal",
     "bytes_to_df",
 ]
